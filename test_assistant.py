eb6d67d315cc60ac137d0181c83c6ef7aae7f684
Saadullah Khan Warsi <saadullah946@gmail.com>
2024-11-18 19:49:39 +0500

    Update test_assistant.py

--- test_assistant.py
+++ test_assistant.py
@@ -3,7 +3,6 @@ from app import system_message
 from langchain.prompts                import ChatPromptTemplate
 from langchain.chat_models            import ChatOpenAI
 from langchain.schema.output_parser   import StrOutputParser
-
 import os
 
 from dotenv import load_dotenv, find_dotenv
@@ -66,5 +65,39 @@ def test_geography_quiz():
       system_message,
       question,
       expected_subjects)
+
+
+def test_assistant_science_quiz():
+    assistant = assistant_chain()
+    question = "Generate a quiz about science."
+    answer = assistant.invoke({"question": question})
+    expected_subjects = ["davinci", "telescope", "physics", "curie"]
+    print(answer)
+    assert any(
+        subject.lower() in answer.lower() for subject in expected_subjects
+    ), f"Expected the assistant questions to include '{expected_subjects}', but it did not"
+
+
+def test_assistant_geography_quiz():
+    assistant = assistant_chain()
+    question = "Generate a quiz about geography."
+    answer = assistant.invoke({"question": question})
+    expected_subjects = ["paris", "france", "louvre"]
+    print(answer)
+    assert any(
+        subject.lower() in answer.lower() for subject in expected_subjects
+    ), f"Expected the assistant questions to include '{expected_subjects}', but it did not"
+
+
+def test_assistant_unknown_subjects():
+    assistant = assistant_chain()
+    question = "Generate a quiz about Rome"
+    answer = assistant.invoke({"question": question})
+    print(answer)
+    # We'll look for a substring of the message the bot prints when it gets a question about any
+    decline_response = "I'm sorry"
+    assert (
+        decline_response.lower() in answer.lower()
+    ), f"Expected the bot to decline with '{decline_response}' got {answer}"
 
 
